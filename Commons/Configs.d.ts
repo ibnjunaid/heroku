786c32a273579f82dcbcf1f2d1bb9629fc22ea1b
ibnjunaid <osamabinjunaid36@gmail.com>
2020-11-29 23:27:43 +0530

    configs adjusted

--- Commons/Configs.d.ts
+++ Commons/Configs.d.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb';
-export declare const URI: string;
-export declare const TWITTER_TOKEN: string;
+export declare const URI = "mongodb+srv://pengooX:Cyanide7Licker8@cluster0.pp3rq.mongodb.net/trends?retryWrites=true&w=majority";
+export declare const TWITTER_TOKEN = "Bearer AAAAAAAAAAAAAAAAAAAAABmeBQEAAAAAmzrYEOjmTefDQ342sQtNLO2eWkI%3DnLjh6by8TpUF6Jx492oP7220NedIGMFlwXno2NRVnIsT1908FL";
 export declare const databaseName = "trends";
 export declare const client: MongoClient;
--- Commons/Configs.js
+++ Commons/Configs.js
@@ -2,8 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.client = exports.databaseName = exports.TWITTER_TOKEN = exports.URI = void 0;
 var mongodb_1 = require("mongodb");
-exports.URI = process.env.ALTAS_MONGO_URI || '';
-exports.TWITTER_TOKEN = process.env.TWITTER_TOKEN || '';
+require('dotenv').config();
+exports.URI = "mongodb+srv://pengooX:Cyanide7Licker8@cluster0.pp3rq.mongodb.net/trends?retryWrites=true&w=majority";
+exports.TWITTER_TOKEN = "Bearer AAAAAAAAAAAAAAAAAAAAABmeBQEAAAAAmzrYEOjmTefDQ342sQtNLO2eWkI%3DnLjh6by8TpUF6Jx492oP7220NedIGMFlwXno2NRVnIsT1908FL";
 exports.databaseName = "trends";
 exports.client = new mongodb_1.MongoClient(exports.URI, { useUnifiedTopology: true });
 //# sourceMappingURL=Configs.js.map
