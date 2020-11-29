66f108833be7a722067affb7a0f5a371e3795402
ibnjunaid <osamabinjunaid36@gmail.com>
2020-11-29 21:05:01 +0530

    package.json changed startup script 2

--- Commons/Configs.js
+++ Commons/Configs.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.client = exports.databaseName = exports.TWITTER_TOKEN = exports.URI = void 0;
 var mongodb_1 = require("mongodb");
-require('dotenv').config();
 exports.URI = process.env.ALTAS_MONGO_URI || '';
 exports.TWITTER_TOKEN = process.env.TWITTER_TOKEN || '';
 exports.databaseName = "trends";
